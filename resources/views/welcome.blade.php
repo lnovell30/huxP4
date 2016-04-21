3340667e2772a597b73669c5b399639aba9a750b
Novell Morris <lnovell30@gmail.com>
2016-04-21 11:13:28 -0400

    Modified the welcome page to include introduction to the application and links to the useful areas of the site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,16 +17,27 @@
 			}
 
 			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
+				width:90%;
+				margin-left:auto;
+				margin-right:auto;
+				border: #d9d9d9 1px solid;
+				border-radius: 4px;
+				padding-left: 20px;
 			}
-
+                        .paragraphContainer {
+			     
+			}
 			.content {
-				text-align: center;
+				
 				display: inline-block;
 			}
-
+			.userGenContainer {
+				
+			}
+                        .subheader {
+			    color:#444;
+			    
+			}
 			.title {
 				font-size: 96px;
 				margin-bottom: 40px;
@@ -40,8 +51,18 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<h1>Developers Best Friend</h1>
+				<div class="paragraphContainer">
+				   <h2 class="subheader">Lorem Ipsum Generator</h2>
+				   <p>In publishing and graphic design, lorem ipsum (derived from Latin dolorem ipsum, translated as "pain itself") is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows viewers to focus on graphic aspects such as font, typography, and page layout without being distracted by the content. It also reduces the need for the designer to come up with meaningful text, as they can instead use quickly-generated lorem ipsum.</p>
+				   <h5>Let's create some Lorem Ipsum (random filler text) for your application...</h5>
+				</div>
+			        <hr>
+			        <div class="userGenContainer">
+				    <h2  class="subheader">Random User Generator</h2>
+				<p> In developing web applications, it is very helpful to be able to have a source of random user generated data... Our system allows you to generate random user data up to maximum of 99 users...</p>
+				  <h5>Let's create your random users...</h5>
+				</div>
 			</div>
 		</div>
 	</body>
